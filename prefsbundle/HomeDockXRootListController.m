1dcae5c08129efe845f2289de9fa3f9b5ba95ca0
Brian Choi <brian9206@hotmail.com>
2018-07-08 16:16:49 +0800

    Finish up preference bundle

--- prefsbundle/HomeDockXRootListController.m
+++ prefsbundle/HomeDockXRootListController.m
@@ -10,4 +10,8 @@
 	return _specifiers;
 }
 
+- (void)respring {
+	system("sleep 1 && killall SpringBoard");
+}
+
 @end
--- src/HomeDockXPreference.m
+++ src/HomeDockXPreference.m
@@ -12,25 +12,26 @@ static HomeDockXPreference *sharedInstance;
 }
 
 + (void)load {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     sharedInstance = [[self alloc] init];
-    [pool release];
 }
 
 - (id) init {
     if ((self = [super init])) {
         NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/pw.ssnull.homedockx.plist"];
-        self.enableHomeGesture = [[prefs objectForKey:@"enableHomeGesture"] boolValue];
-        self.enableDock = [[prefs objectForKey:@"enableDock"] boolValue];
-        self.noKeyboard = [[prefs objectForKey:@"noKeyboard"] boolValue];
-        self.enableInAppDock = [[prefs objectForKey:@"enableInAppDock"] boolValue];
-        self.enableHomeButtonDismiss = [[prefs objectForKey:@"enableHomeButtonDismiss"] boolValue];
-        self.enableDockSuggestion = [[prefs objectForKey:@"enableDockSuggestion"] boolValue];
-        self.showRecentApp = [[prefs objectForKey:@"showRecentApp"] boolValue];
-        self.maxSuggestion = [[prefs objectForKey:@"maxSuggestion"] intValue];
+        
+        if (prefs) {
+            self.enableHomeGesture = [[prefs objectForKey:@"enableHomeGesture"] boolValue];
+            self.enableDock = [[prefs objectForKey:@"enableDock"] boolValue];
+            self.noKeyboard = [[prefs objectForKey:@"noKeyboard"] boolValue];
+            self.enableInAppDock = [[prefs objectForKey:@"enableInAppDock"] boolValue];
+            self.enableHomeButtonDismiss = [[prefs objectForKey:@"enableHomeButtonDismiss"] boolValue];
+            self.enableDockSuggestion = [[prefs objectForKey:@"enableDockSuggestion"] boolValue];
+            self.showRecentApp = [[prefs objectForKey:@"showRecentApp"] boolValue];
+            self.maxSuggestion = [[prefs objectForKey:@"maxSuggestion"] intValue];
 
-        if (self.maxSuggestion <= 0) {
-            self.maxSuggestion = 2;
+            if (self.maxSuggestion <= 0) {
+                self.maxSuggestion = 2;
+            }
         }
     }
     
